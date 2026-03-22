90ecc01d5e83c7608d0dacc872bf24a2f0b849fb
arda konyali <konyali.arda@ibu.edu.mk>
2026-03-22 15:28:56 +0100

    week 2

--- 2.c
+++ 2.c
@@ -1,28 +1,81 @@
 #include <cs50.h>
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
+#include <string.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
-    int height;
+    string text = get_string("Text: ");
 
-    do
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    float L = ((float) letters / words) * 100;
+    float S = ((float) sentences / words) * 100;
+
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 1)
     {
-        height = get_int("Height: ");
+        printf("Before Grade 1\n");
     }
-    while (height < 1 || height > 8);
-
-    for (int i = 1; i <= height; i++)
+    else if (index >= 16)
     {
-        for (int j = 0; j < height - i; j++)
-        {
-            printf(" ");
-        }
-
-        for (int k = 0; k < i; k++)
-        {
-            printf("#");
-        }
-
-        printf("\n");
+        printf("Grade 16+\n");
     }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
+
+int count_letters(string text)
+{
+    int count = 0;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int count_words(string text)
+{
+    int count = 1;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == ' ')
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int count_sentences(string text)
+{
+    int count = 0;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+
+    return count;
 }
--- 4.c
+++ 4.c
@@ -1,28 +1,69 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+bool valid_key(string key);
+
+int main(int argc, string argv[])
 {
-    int cents;
-
-    do
+    if (argc != 2 || !valid_key(argv[1]))
     {
-        cents = get_int("Change owed: ");
+        printf("Usage: ./substitution key\n");
+        return 1;
     }
-    while (cents < 0);
 
-    int coins = 0;
+    string key = argv[1];
+    string plaintext = get_string("plaintext:  ");
 
-    coins += cents / 25;
-    cents %= 25;
+    printf("ciphertext: ");
 
-    coins += cents / 10;
-    cents %= 10;
+    for (int i = 0, n = strlen(plaintext); i < n; i++)
+    {
+        char c = plaintext[i];
 
-    coins += cents / 5;
-    cents %= 5;
+        if (isupper(c))
+        {
+            int index = c - 'A';
+            printf("%c", toupper(key[index]));
+        }
+        else if (islower(c))
+        {
+            int index = c - 'a';
+            printf("%c", tolower(key[index]));
+        }
+        else
+        {
+            printf("%c", c);
+        }
+    }
 
-    coins += cents;
+    printf("\n");
+    return 0;
+}
 
-    printf("%i\n", coins);
+bool valid_key(string key)
+{
+    if (strlen(key) != 26)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            return false;
+        }
+
+        for (int j = i + 1; j < 26; j++)
+        {
+            if (tolower(key[i]) == tolower(key[j]))
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
 }
